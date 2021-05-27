2c862f674306308208d2d03eca6a56d5b0220d91
DanujaV <danujagreru@gmail.com>
2021-05-27 15:40:46 +0530

    complete how to get values from UI to API using header and body

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const bodyParser = require('body-parser')
+
 const customer = require('./routes/customer-routs')
 const item = require('./routes/item-routs')
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.use('/customer', customer)
 app.use('/item', item)
--- routes/customer-routs.js
+++ routes/customer-routs.js
@@ -1,7 +1,9 @@
-const { Router } = require('express')
+const { Router, request } = require('express')
 const express = require('express')
 const router = express.Router()
- 
+
+
+
 router.get('/', (req, res) => {
     console.log('execute get')
     res.send('Customer get requets recieved!')
@@ -9,11 +11,16 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     console.log('execute post')
+
+    /* get values through headers */
+    console.log(req.headers.id + "-" + req.headers.name + "-" + req.headers.address)
     res.send('Customer post requets recieved!')
 })
 
 router.put('/', (req, res) => {
     console.log('execute put')
+    /* get request values through body */
+    console.log(req.body.id + " - " + req.body.name + " - " + req.body.address)
     res.send('Customer put requets recieved!')
 })
 
